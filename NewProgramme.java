e358c47f58806fd19c7999077b0f9d27fc9dc116
Vijiya <vijayaarjun47102@gmail.com>
2024-12-27 15:58:58 +0530

    Second commit

---
+++ NewProgramme.java
@@ -0,0 +1,24 @@
+class F{
+
+        public static void main(String[] args){
+        System.out.println("1 2 3 4 5 6 7 8 9 10");
+}
+
+
+}
+class M{
+
+        public static void main(String[] args){
+        System.out.println("1 2 3 4 5 6 7 8 9 10");
+}
+
+
+}
+class N{
+
+        public static void main(String[] args){
+        System.out.println("10 9 8 7 6 5 4 3 2 1");
+}
+
+
+}
